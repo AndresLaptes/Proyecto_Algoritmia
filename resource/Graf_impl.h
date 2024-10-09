fd44d1af6fc0fa680fbeba46863b6dd50eff8eac
Andres Lucian Laptes Costan <alaptes.210404@gmail.com>
2024-10-09 20:03:16 +0200

    Nueva estrutura de datos

--- resource/Graf_impl.h
+++ resource/Graf_impl.h
@@ -9,178 +9,112 @@
 #include <stdexcept>
 using namespace std;
 
-template <typename T>
-Graf<T>::Graf(const T& value) {
-    Node *nuevo = new Node;
-    nuevo->vertice = value;
-    vertices.push_front(nuevo);
+
+grafo::grafo() {
+
 }
 
-
-template <typename T> 
-Graf<T>::~Graf() {
-    for (auto& node : vertices) delete node;
-    vertices.clear();
+grafo::grafo(int v, const list<int>& arestas) {
+    list<int> aux = arestas;
+    vertices.push_back({v,aux});
 }
 
-template <typename T>
-Graf<T>::Graf() {}
-
-template <typename T>
-int Graf<T>::size() const {
-    return vertices.size();
+void grafo::insert_vertice(int v) {
+    list<int> aux;
+    vertices.push_back({v,aux});
 }
 
-template <typename T>
-bool Graf<T>::exist(const T& val) const {
-    for (auto it : vertices) {
-        if (it->vertice == val) return true;
+void grafo::remove_vertice(int v) {
+    for (auto it = vertices.begin(); it != vertices.end(); ++it) {
+        if ((*it).first == v) {
+            vertices.erase(it);
+        }
+    }
+}
+
+void grafo::insert_aresta(int v1, int v2) {
+    if (not exist_conection(v1, v2) and exist(v1) and exist(v2)) {
+        for (auto it = vertices.begin(); it != vertices.end(); ++it) {
+            if ((*it).first == v1) (*it).second.push_back(v2);
+            else if ((*it).first == v2) (*it).second.push_back(v1);
+        }
+    }
+}
+
+bool grafo::exist_conection(int v1, int v2) const {
+    for (auto& it1 : vertices) {
+        if (it1.first == v1) {
+            for (auto& it2 : it1.second) {
+                if (it2 == v2) return true;
+            }
+        }
     }
     return false;
 }
 
-template <typename T>
-void Graf<T>::insert_vertice(const T& val, Node* &aux) {
-    Node *nuevo = new Node;
-    nuevo->vertice = val;
-    vertices.push_front(nuevo);
-    aux = nuevo;
-}
-
-template <typename T>
-void Graf<T>::remove_vertice(const T& val) {
+void grafo::remove_aresta(int v1, int v2) {
     for (auto it = vertices.begin(); it != vertices.end(); ++it) {
-        if ((*it)->vertice == val) {
-            delate (*it);
-            vertices.erase(it);
-            break;
-        }
-    }
-}
-
-template <typename T>
-void Graf<T>::insert_aresta(const T& a, const T&b) {
-    bool found = false;
-    if (a == b) {
-        string msg = "Error: son el mismo vertice";
-        throw invalid_argument(msg);
-    }
-
-
-    Node *A = nullptr;
-    Node *B = nullptr;
-    for (auto& node : vertices) {
-        if (node->vertice == a) A = node;
-        else if (node->vertice == b) B = node;
-    }
-
-    if (A == nullptr) {
-        insert_vertice(a, A);
-    }
-    if (B == nullptr) {
-        insert_vertice(b, B);
-    }
-
-    for (auto& it : A->arestas) {
-        if (it == B) {
-            found = true;
-            break;
-        }
-    }
-
-    if (not found) {
-        A->arestas.push_front(B);
-        B->arestas.push_front(A);
-    }
-} 
-
-template <typename T> 
-void Graf<T>::remove_aresta(const T& a, const T& b) {
-        bool found = false;
-    if (a == b) {
-        string msg = "Error: son el mismo  vertice";
-        throw invalid_argument(msg);
-    }
-
-
-    Node *A = nullptr;
-    Node *B = nullptr;
-    for (auto& node : vertices) {
-        if (node->vertice == a) A = node;
-        else if (node->vertice == b) B = node;
-    }
-
-    if (A == nullptr) {
-        string msg = "Error: tu primer nodo no existe";
-        throw invalid_argument(msg);
-    }
-
-    if (B == nullptr) {
-        string msg = "Error: tu segundo nodo no existe";
-        throw invalid_argument(msg);
-    }
-
-    for (auto& it : A->conections) {
-        if (it == B) {
-            delete it;
-            found = true;
-            break;
-        }
-    }
-
-    if (found) {
-        for (auto& it : B->conections) {
-            if (it == A) {
-                delete it;
-                found = true;
-                break;
+        if ((*it).first == v1) {
+            for (auto it2 = (*it).second.begin(); it2 != (*it).second.end(); ++it2) {
+                if ((*it2) == v2) (*it).second.erase(it2);
             }
         }
-    } else throw invalid_argument("Error: tu aresta entre los vertices dados no existe");
+
+        if ((*it).first == v2) {
+            for (auto it2 = (*it).second.begin(); it2 != (*it).second.end(); ++it2) {
+                if ((*it2) == v1) (*it).second.erase(it2);
+            }
+        }
+    }
 }
 
-template <typename T> 
-void Graf<T>::read(ifstream& file) {
+void grafo::read(ifstream& file) {
     string linea;
-    while (getline(file, linea)) {
+    while(getline(file, linea)) {
         if (not linea.empty()) {
             stringstream line(linea);
 
             string vertice;
             getline(line, vertice, ',');
-            Node *nosirve = new Node;
-            if (not exist(stringToT(vertice))) insert_vertice(stringToT(vertice), nosirve);
-
+            if (not exist(stoi(vertice))) insert_vertice(stoi(vertice));
+            
             string arestas;
             while (getline(line, arestas, ',')) {
-                insert_aresta(stringToT(vertice), stringToT(arestas));
+                insert_aresta(stoi(vertice), stoi(arestas));
             }
-            
         } else break;
     }
 }
 
-template <typename T>
-T Graf<T>::stringToT(const string& a) {
-    T value;
-    std::istringstream iss(a);
-    iss >> value;
-
-    if (iss.fail()) {
-        throw std::invalid_argument("Error al convertir string a tipo T.");
+void grafo::write(ofstream& file) {
+    for (auto& vertice : vertices) {
+        file << to_string(vertice.first);
+        for (auto& aresta : vertice.second) {
+            file << "," << to_string(aresta);
+        }
+        file << endl;
     }
-
-    return value;
 }
 
-template <typename T>
-void Graf<T>::print() {
-    for (auto& nodo : vertices) {
-        cout << "En la direccion (" << nodo << ") hay un nodo con valor: " << nodo->vertice << " con las siguientes conexiones : ";
-        auto end = nodo->arestas.end();
-        for (auto it = nodo->arestas.begin(); it != end; ++it) cout << (*it)->vertice << " ";
-        cout << endl; 
+void grafo::print() const {
+    for (auto& v : vertices) {
+        cout << "Soy " << v.first << " con numero de arestas " << v.second.size() << " y estamos conectado a : ";
+        for (auto& a : v.second) {
+            cout << a << " ";
+        }
+        cout << endl;
     }
+}
+
+int grafo::size() const {
+    return vertices.size();
+}
+
+bool grafo::exist(int v) const {
+    for (auto& i : vertices) {
+        if (i.first == v) return true;
+    }
+    return false;
 }
 
 #endif
--- rsc/main.cpp
+++ rsc/main.cpp
@@ -1,20 +1,33 @@
-#include <graf.h>
+#include "graf.h"
 #include <vector>
+#include <fstream>
+#include <sstream>
 #include <iostream>
 using namespace std;
 
 int main() {
     ifstream file("grafos.csv");
     if (not file.is_open()) cout << "No se puede abrir" << endl; 
+    vector<grafo> a(2); 
     for (int i = 0; i < 2; ++i) {
-        Graf<int> actual;
-        actual.read(file);
-        cout << "Grafo: "<< i << endl;
-        actual.print();
-        cout << endl;
+        grafo aux;
+        aux.read(file);
+        a[i] = aux; 
     }
 
+    cout << "Acabamos lectura" << endl;
+    for (int i = 0; i < 2; ++i) a[i].print();
     file.close();
 
+    for (int i = 0; i < 2; ++i) {
+        string name = "grafo";
+        name += to_string(i) + ".csv";
+        ofstream nuevo(name);
+        a[i].write(nuevo);
+        nuevo.close(); 
+    }
+
+
     cout << "Acabado" << endl; 
+    
 } 
