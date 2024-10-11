802a564b2cc39062e54bf3307c79eabd740db757
enric.segarra <enric.segarra@estudiantat.upc.edu>
2024-10-11 11:38:00 +0200

    main canvi

--- rsc/main.cpp
+++ rsc/main.cpp
@@ -1,33 +1,33 @@
-#include "graf.h"
-#include <vector>
-#include <fstream>
-#include <sstream>
 #include <iostream>
+#include <vector>
+#include "Graf_impl.h"
 using namespace std;
+
+//Genera un graf amb n vèrtex i amb la probabilitat p de generar arestes entre vèrtexs
+ Graf<int> genera_random(int n, float p) {
+    Graf<int> G;
+    int pr = p*100;
+    for (int i = 0; i < n; ++i) G.insert_vertice(i);  //Afegeix el node i al graf
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            int r = rand()%100;
+            if (r < pr) G.insert_aresta(i,j); //Afegeix al node i una aresta al node j
+        }
+    }
+    return G;
+}
 
 int main() {
     ifstream file("grafos.csv");
     if (not file.is_open()) cout << "No se puede abrir" << endl; 
-    vector<grafo> a(2); 
     for (int i = 0; i < 2; ++i) {
-        grafo aux;
-        aux.read(file);
-        a[i] = aux; 
+        Graf<int> actual = genera_random(10,0.5);
+        cout << "Grafo: "<< i << endl;
+        actual.print();
+        cout << endl;
     }
 
-    cout << "Acabamos lectura" << endl;
-    for (int i = 0; i < 2; ++i) a[i].print();
     file.close();
 
-    for (int i = 0; i < 2; ++i) {
-        string name = "grafo";
-        name += to_string(i) + ".csv";
-        ofstream nuevo(name);
-        a[i].write(nuevo);
-        nuevo.close(); 
-    }
-
-
     cout << "Acabado" << endl; 
-    
 } 
